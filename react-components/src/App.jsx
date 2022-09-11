4f223fc8e335c6896ada4f2cd72b8f93f9a63584
Max <12maks32@gmail.com>
2022-09-11 21:04:08 +0300

    Add plus & minus buttons

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -9,15 +9,20 @@ class App extends React.Component {
     }
   }
 
-  handleClick = () => {
-    this.setState({count: this.state.count + 1})
+  handleClickPlus = () => {
+      this.setState({count: this.state.count + 1})
   }
+  handleClickMinus = () => {
+    this.setState({count: this.state.count - 1})
+}
 
   render() {
     return (
       <div className="App">
         Welcome from React
-        <button onClick={this.handleClick}>{this.state.count}</button>
+        <button onClick={this.handleClickMinus}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleClickPlus}>+</button>
       </div>
     )
   }
