37e3433f8cf2e4075fa1ca439395cf6f1e73e501
Max <12maks32@gmail.com>
2022-09-11 20:56:01 +0300

    Add button to App

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      count: 0,
+      someKey: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({count: this.state.count + 1})
   }
 
   render() {
     return (
       <div className="App">
-        Hello from React
+        Welcome from React
+        <button onClick={this.handleClick}>{this.state.count}</button>
       </div>
     )
   }
