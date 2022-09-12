2e29ce2a9c1db910715f8a6aa51d42bef82360f1
Max <12maks32@gmail.com>
2022-09-12 15:44:02 +0300

    Made timer & add tailwind to project

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -4,25 +4,50 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: 0,
-      someKey: false
+      seconds: 0,
+      isTimerWorks: false,
     }
   }
 
-  handleClickPlus = () => {
-      this.setState({count: this.state.count + 1})
+  startTimer = () => {
+    this.setState({isTimerWorks: true})
+    console.log('Timer works')
+    this.timerId = setInterval(() => {
+      let second = this.state.seconds + 1
+      this.setState({seconds: second})
+      localStorage.setItem('timerSeconds', second)
+    }, 1000)
   }
-  handleClickMinus = () => {
-    this.setState({count: this.state.count - 1})
-}
+
+  componentDidMount() {
+    if (localStorage.getItem('timerSeconds')) {
+      this.setState({seconds: localStorage.getItem('timerSeconds')})
+    }
+    console.log('componentDidMount')
+  }
+
+  stopTimer = () => {
+    clearInterval(this.timerId)
+    this.setState({isTimerWorks: false})
+  }
+
+  resetTimer = () => {
+    this.setState({seconds: 0})
+    localStorage.removeItem('timerSeconds')
+  }
+
 
   render() {
     return (
       <div className="App">
-        Welcome from React
-        <button onClick={this.handleClickMinus}>-</button>
-        <span>{this.state.count}</span>
-        <button onClick={this.handleClickPlus}>+</button>
+        React Timer
+        <span>{this.state.seconds}</span>
+        {this.state.isTimerWorks ?
+          <button  onClick={this.stopTimer}>Stop</button>
+          :
+          <button  onClick={this.startTimer}>Start</button>
+        }
+        <button onClick={this.resetTimer}>Reset</button>
       </div>
     )
   }
