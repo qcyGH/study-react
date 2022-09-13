943e601313d44160df7c2cbbc939baee710d0af8
Max <12maks32@gmail.com>
2022-09-13 13:43:29 +0300

    Added human readable time format

--- react-components/src/Timer.jsx
+++ react-components/src/Timer.jsx
@@ -11,7 +11,6 @@ class Timer extends React.Component {
 
   startTimer = () => {
     this.setState({isTimerWorks: true})
-    console.log('Timer works')
     this.timerId = setInterval(() => {
       let second = this.state.seconds + 1
       this.setState({seconds: second})
@@ -23,7 +22,6 @@ class Timer extends React.Component {
     if (localStorage.getItem('timerSeconds')) {
       this.setState({seconds: parseInt(localStorage.getItem('timerSeconds'))})
     }
-    console.log('componentDidMount')
   }
 
   stopTimer = () => {
@@ -36,20 +34,33 @@ class Timer extends React.Component {
     localStorage.removeItem('timerSeconds')
   }
 
+  // Time format
+
+  humanReadable (seconds) {
+    let pad = function(x) { return (x < 10) ? "0"+x : x; }
+    return pad(parseInt(seconds / (60*60))) + ":" +
+           pad(parseInt(seconds / 60 % 60)) + ":" +
+           pad(seconds % 60)
+  }
+
+  // Time format
+
 
   render() {
     return (
       <section className='Timer h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500'>
-        <div className='timer-wrapper flex flex-col items-center w-11/12 sm:w-9/12 min-w-max py-40 bg-stone-900 text-slate-50 rounded-3xl shadow-2xl'>
-          <h1 className='font-mono text-4xl tracking-wide text-inherit'>React Timer</h1>
-          <span className='font-mono text-2xl my-5 text-inherit bg-blue-700 px-3 py-1 rounded-full'>{this.state.seconds}</span>
-          <div className='timer__buttons-wrapper w-full flex flex-row justify-center mt-4'>
-            {this.state.isTimerWorks ?
-              <button className='font-mono text-lg bg-blue-900 text-slate-100 py-1 px-4 rounded-lg shadow shadow-blue-900/50 transition-all duration-150  ease-in hover:shadow-blue-900/80 hover:bg-blue-800 active:shadow-none' onClick={this.stopTimer}>Stop</button>
-              :
-              <button className='font-mono text-lg bg-blue-800 text-slate-100 py-1 px-4 rounded-lg shadow shadow-blue-800/50 transition-all duration-150  ease-in hover:shadow-blue-800/80 hover:bg-blue-700 active:shadow-none' onClick={this.startTimer}>Start</button>
-            }
-            <button className='font-mono text-lg bg-indigo-50 text-black py-1 px-4 rounded-lg ml-3 shadow shadow-indigo-50/50 transition-all duration-150 ease-in hover:shadow-indigo-100/80 hover:bg-white active:shadow-none' onClick={this.resetTimer}>Reset</button>
+        <div className='timer-wrapper flex flex-col items-center w-11/12 sm:w-9/12 h-3/5 min-w-max py-4 bg-stone-900 text-slate-50 rounded-3xl shadow-2xl shadow-black/50'>
+          <h1 className='font-mono text-5xl tracking-wide text-inherit mt-2'>React Timer</h1>
+          <div className='flex flex-col my-auto'>
+            <span className='font-mono text-5xl my-5 text-inherit bg-blue-700 px-3 py-1 rounded-full shadow-sm shadow-blue-900'>{this.humanReadable(this.state.seconds)}</span>
+            <div className='timer__buttons-wrapper w-full flex flex-row justify-center mt-4'>
+              {this.state.isTimerWorks ?
+                <button className='font-mono text-lg bg-blue-900 text-slate-100 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-blue-900/50 transition-all duration-150  ease-in hover:shadow-blue-900/80 hover:bg-blue-800 active:shadow-none' onClick={this.stopTimer}>Stop</button>
+                :
+                <button className='font-mono text-lg bg-blue-800 text-slate-100 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-blue-800/50 transition-all duration-150  ease-in hover:shadow-blue-800/80 hover:bg-blue-700 active:shadow-none' onClick={this.startTimer}>Start</button>
+              }
+              <button className='font-mono text-lg bg-indigo-50 text-black py-1 px-4 rounded-lg hover:rounded-xl ml-3 shadow shadow-indigo-50/50 transition-all duration-150 ease-in hover:shadow-indigo-100/80 hover:bg-white active:shadow-none' onClick={this.resetTimer}>Reset</button>
+            </div>
           </div>
         </div>
       </section>
