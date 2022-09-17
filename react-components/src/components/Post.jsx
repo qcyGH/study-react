7caae4ede93a9162a515585d15c895d9bc1efa52
Max <12maks32@gmail.com>
2022-09-17 21:16:18 +0300

    Font size increased for List app

--- react-components/src/components/Post.jsx
+++ react-components/src/components/Post.jsx
@@ -1,9 +1,9 @@
 export function Post (props) {
-    return <h2 className="font-sans my-3 mx-5 text-lg">
+    return <h2 className="font-sans my-3 mx-5 text-3xl">
         {props.name}
         <button
             onClick={e => props.func(props.id)}
-            className="font-mono text-lg bg-rose-800 text-slate-100 mx-3 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-rose-800/50 transition-all duration-150  ease-in hover:shadow-rose-800/80 hover:bg-rose-700 active:shadow-none"
+            className="font-mono text-2xl bg-rose-800 text-slate-100 mx-3 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-rose-800/50 transition-all duration-150  ease-in hover:shadow-rose-800/80 hover:bg-rose-700 active:shadow-none"
         >
             Delete
         </button>
