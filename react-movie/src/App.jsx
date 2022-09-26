38a4cc83e83cd8736675a869a3a994585d25c737
Max <12maks32@gmail.com>
2022-09-26 15:29:50 +0300

    Upgrade styles

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -32,7 +32,7 @@ class App extends React.Component {
             <section className='movie-app bg-zinc-100 py-5 px-10'>
                 <div className='flex items-center justify-center'>
                     <input
-                        className='font-mono text-xl text-zinc-50 bg-zinc-800 rounded-lg border-none py-1 px-2 focus-visible:outline focus-visible:outline-indigo-500 focus:outline-4 focus:border-none focus:shadow-none focus:ring-0 focus:outline-offset-0'
+                        className='font-mono text-xl text-zinc-50 bg-zinc-800 rounded-lg border-none py-1 px-2 focus-visible:outline focus-visible:outline-sky-500 focus:outline-4 focus:border-none focus:shadow-none focus:ring-0 focus:outline-offset-0'
                         onChange={e => this.handleChange(e)}
                         type='text'
                         name='movieName'
@@ -41,7 +41,7 @@ class App extends React.Component {
                     />
                     <button
                         onClick={this.fetchMovies}
-                        className="font-mono text-2xl bg-zinc-200 text-slate-900 mx-3 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-sky-50/50 transition-all duration-150  ease-in hover:shadow-sky-50/80 hover:bg-sky-200 active:shadow-none focus-visible:outline focus-visible:outline-indigo-500 outline-offset-0"
+                        className="font-mono text-2xl bg-zinc-200 text-slate-900 mx-3 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-sky-50/50 transition-all duration-150 ease-in hover:shadow-sky-50/80 hover:bg-sky-200 active:shadow-none focus-visible:outline focus-visible:outline-sky-500 outline-4 outline-offset-0"
                     >
                         Search
                     </button>
