d3978afc6724814ac397ba02ae13f59ad5a226ed
Max <12maks32@gmail.com>
2022-09-28 20:57:11 +0300

    Made search more pretty and comfortable
    Change styles and jsx for search. Added search by pressing Enter. If nothing found will show notification

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -7,6 +7,7 @@ class App extends React.Component {
         this.state = {
             movieList: [],
             movieName: '',
+            isMovieFound: true,
         }
     }
 
@@ -14,38 +15,61 @@ class App extends React.Component {
         let apiKey = '328a7cff'
         let url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${this.state.movieName}`
 
-        fetch(url)
-            .then((response) => {
-                return response.json()
-            })
-            .then((data) => {
-                this.setState({movieList: data.Search})
-            })
+        if (this.state.movieName !== '') {
+            fetch(url)
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    if (data.Search !== undefined) {
+                        this.setState({movieList: data.Search})
+                        this.setState({isMovieFound: true})
+                    } else {
+                        this.setState({movieList: []})
+                        this.setState({isMovieFound: false})
+                    }
+                })
+        }
     }
 
     handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value}) // використовуємо [e.target.name], щоб динамічно пов'язувати input з state, та не писати для кожного input свою функцію
+        this.setState({[e.target.name]: e.target.value})
     }
+
+    keyPress = (e) => {
+        if (e.code === 'Enter') {
+            this.fetchMovies()
+        }
+    }
+
 
     render() {
         return (
             <section className='movie-app bg-zinc-100 py-5 px-10'>
                 <div className='flex items-center justify-center'>
-                    <input
-                        className='font-mono text-xl text-zinc-50 bg-zinc-800 rounded-lg border-none py-1 px-2 focus-visible:outline focus-visible:outline-sky-500 focus:outline-4 focus:border-none focus:shadow-none focus:ring-0 focus:outline-offset-0'
-                        onChange={e => this.handleChange(e)}
-                        type='text'
-                        name='movieName'
-                        placeholder='Task text'
-                        value={this.movieName}
-                    />
-                    <button
-                        onClick={this.fetchMovies}
-                        className="font-mono text-2xl bg-zinc-200 text-slate-900 mx-3 py-1 px-4 rounded-lg hover:rounded-xl shadow shadow-sky-50/50 transition-all duration-150 ease-in hover:shadow-sky-50/80 hover:bg-sky-200 active:shadow-none focus-visible:outline focus-visible:outline-sky-500 outline-4 outline-offset-0"
-                    >
-                        Search
-                    </button>
+                    <label className='relative w-4/5 lg:w-1/5 h-[36px] flex items-center justify-center divide-x divide-zinc-600 bg-zinc-800 rounded-lg shadow shadow-zinc-400/50 transition-all duration-200 ease-in focus-within:w-full lg:focus-within:w-3/5'>
+                        <input
+                            className='peer absolute left-1/2 w-full lg:w-3/5 translate-x-[-50%] text-center text-xl text-zinc-50 bg-zinc-800 rounded-lg border-none py-1 px-2 focus:border-none focus:shadow-none focus:ring-0'
+                            onChange={e => this.handleChange(e)}
+                            onKeyDown={e => this.keyPress(e)}
+                            type='text'
+                            name='movieName'
+                            placeholder='Search'
+                            value={this.movieName}
+                        />
+                        <button
+                            onClick={this.fetchMovies}
+                            className="absolute right-0 invisible peer-focus-visible:visible text-slate-200 py-1 px-3"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                            </svg>
+                        </button>
+                    </label>
                 </div>
+                { !this.state.isMovieFound &&
+                 <div className='text-2xl text-zinc-600 text-center mt-10'>No movies for your request 😒</div>
+                }
                 <List cards={this.state.movieList}></List>
             </section>
         )
