c6ca830ab86ccdb03b049bce5f41ef4c9a8d784f
Max <12maks32@gmail.com>
2022-09-28 21:13:43 +0300

    Made search button show slowly

--- react-movie/src/App.jsx
+++ react-movie/src/App.jsx
@@ -59,7 +59,7 @@ class App extends React.Component {
                         />
                         <button
                             onClick={this.fetchMovies}
-                            className="absolute right-0 invisible peer-focus-visible:visible text-slate-200 py-1 px-3"
+                            className="absolute right-0 invisible opacity-0 peer-focus-visible:visible peer-focus-visible:opacity-100 duration-200 ease text-slate-200 py-1 px-3"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
