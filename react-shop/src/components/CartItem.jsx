2f2ef6a88889018fb0512dc6656befbfc6d31f7b
Max <12maks32@gmail.com>
2022-10-20 23:54:51 +0300

    Added styles to cart modal

--- react-shop/src/components/CartItem.jsx
+++ react-shop/src/components/CartItem.jsx
@@ -9,12 +9,13 @@ export function CartItem(props) {
         quantity,
     } = props
 
+    // <ul className= 'grid grid-flow-col grid-col-5 gap-4 py-2 px-4 mb-2 bg-zinc-200/30 dark:bg-zinc-900/30 hover:bg-zinc-200/90 dark:hover:bg-zinc-900/90 rounded-md'>
     return (
-        <div className='flex'>
-            <span>{name}</span>
-            <span>{description}</span>
-            <span>{finalPrice}</span>
-            <span>{quantity}</span>
-        </div>
+        <>
+            <div className='pl-2 pr-4 py-1 bg-zinc-200/30 dark:bg-zinc-900/30 hover:bg-zinc-200/90 dark:hover:bg-zinc-900/90 rounded-md'>{name}</div>
+            <div className='pl-2 pr-4 py-1 bg-zinc-200/30 dark:bg-zinc-900/30 hover:bg-zinc-200/90 dark:hover:bg-zinc-900/90 rounded-md'>{description}</div>
+            <div className='pl-2 pr-4 py-1 bg-zinc-200/30 dark:bg-zinc-900/30 hover:bg-zinc-200/90 dark:hover:bg-zinc-900/90 rounded-md'>{finalPrice}</div>
+            <div className='pl-2 pr-4 py-1 bg-zinc-200/30 dark:bg-zinc-900/30 hover:bg-zinc-200/90 dark:hover:bg-zinc-900/90 rounded-md'>{quantity}</div>
+        </>
     )
 }
--- react-shop/src/components/CartModal.jsx
+++ react-shop/src/components/CartModal.jsx
@@ -6,19 +6,28 @@ export function CartModal(props) {
     const { items = [] } = props
 
     return (
-        <div className='absolute'>
-            {
-                items.map(item => (
-                    <CartItem
-                     name={item.name}
-                     id={item.id}
-                     key={item.id}
-                     description={item.description}
-                     finalPrice={item.finalPrice}
-                     quantity={item.quantity}
-                    />
-                ))
-            }
+        <div className='absolute bottom-[-1rem] right-0 translate-y-[100%] w-max
+                        bg-zinc-300/90 dark:bg-zinc-800/90 p-2 rounded-md backdrop-blur-xl
+                        text-zinc-900 dark:text-zinc-200
+        '>
+            <div className='grid auto-rows-max grid-cols-4 gap-x-4 gap-y-2.5 py-2 px-4'>
+                <div>Name</div>
+                <div>Description</div>
+                <div>Price</div>
+                <div>Quantity</div>
+                {
+                    items.map(item => (
+                        <CartItem
+                        name={item.name}
+                        id={item.id}
+                        key={item.id}
+                        description={item.description}
+                        finalPrice={item.finalPrice}
+                        quantity={item.quantity}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
