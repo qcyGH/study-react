c22dee9fb701726d1a4efe6f461f81691037cd34
Max <12maks32@gmail.com>
2022-10-22 16:25:02 +0300

    Added total price for modal

--- react-shop/src/components/CartModal.jsx
+++ react-shop/src/components/CartModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useContext } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 import { Icon, IconButton } from '@chakra-ui/react'
 import { CloseIcon } from '@chakra-ui/icons'
 import { OrderContext } from '../hooks/OrderContext'
@@ -6,7 +6,22 @@ import { CartItem } from './CartItem'
 
 export function CartModal(props) {
     const { items = [], show, closeModalOutside, closeModal } = props
+    const [totalPrice, setTotalPrice] = useState(0)
     const rootCartModal = useRef(null)
+
+    const calcTotalPrice = () => {
+        let newTotalPrice = 0
+
+        items.forEach(item => {
+            newTotalPrice += item.finalPrice * item.quantity
+        })
+
+        setTotalPrice(newTotalPrice)
+    }
+
+    useEffect(() => {
+        calcTotalPrice()
+    }, [items])
 
     useEffect(() => {
         if (!show) return
@@ -27,7 +42,7 @@ export function CartModal(props) {
 
     return (
         <div ref={rootCartModal}
-            className=' absolute bottom-[-1rem] right-0 translate-y-[100%] w-max p-2 pt-6 pr-4
+            className=' absolute bottom-[-1rem] right-0 translate-y-[100%] w-max p-2 pr-4
                         bg-zinc-300/90 dark:bg-zinc-800/90 rounded-md backdrop-blur-xl backdrop-saturate-150
                         shadow-lg shadow-zinc-400/50 dark:shadow-zinc-900/50
                         text-zinc-900 dark:text-zinc-200
@@ -35,26 +50,29 @@ export function CartModal(props) {
             '>
             {
                 items.length > 0
-                ? <div className='grid auto-rows-max grid-cols-4 gap-x-4 gap-y-2.5 py-2 px-4'>
-                    <div className='pl-1'>Name</div>
-                    <div className='pl-1'>Description</div>
-                    <div className='pl-1'>Price</div>
-                    <div className='pl-1'>Quantity</div>
-                    {
-                        items.map(item => (
-                            <CartItem
-                            item={item}
-                            key={item.id}
-                            />
-                        ))
-                    }
+                ? <div className='pb-2'>
+                    <div className='grid auto-rows-max grid-cols-4 gap-x-4 gap-y-2.5 py-4 px-4'>
+                        <div className='pl-1'>Name</div>
+                        <div className='pl-1'>Description</div>
+                        <div className='pl-1'>Price</div>
+                        <div className='pl-1'>Quantity</div>
+                        {
+                            items.map(item => (
+                                <CartItem
+                                item={item}
+                                key={item.id}
+                                />
+                            ))
+                        }
+                    </div>
+                    <span className='block text-right pr-2'>Total price: {totalPrice}</span>
                 </div>
-                : <span>Cart is empty</span>
+                : <span className='pr-6'>Cart is empty</span>
             }
             <span className='absolute top-1 right-1 flex'>
                 <IconButton
                     aria-label='close modal'
-                    style={{padding: 10}}
+                    style={{padding: 11}}
                     onClick={(e) => closeModal(e)}
                     icon={<CloseIcon w={10} h={10}/>}
                 >
